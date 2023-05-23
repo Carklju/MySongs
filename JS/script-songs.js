93445a679520b02f6eac45ccb7dec3f8e2960b77
Ivica Kljucar <ivicamacbook@Ivicas-MacBook-Air.local>
2023-05-23 15:59:46 +0200

    prikazi pesme

--- JS/script-songs.js
+++ JS/script-songs.js
@@ -8,15 +8,26 @@ function prikaziPesme(data) {
   for (const item of instrumenti) {
     item.addEventListener("click", (e) => {
       let naslov = e.target.innerText;
-      
-      for (let song of data.songs) {
-            console.log("radi");
-            window.location.replace("../HTML/instrument.html");
-            let listaPesama = document.querySelector(".song-list");
+      if (naslov === "Bass Guitar") {
+        let filtriranoBass = data.songs.filter((song) => {
+          return song.instrument === "Bass Guitar"; 
+        });
+        let stranica = document.querySelector(".instruments");
+        stranica.style.display = "none";
+        console.log(filtriranoBass);
+        for (let song of filtriranoBass) {
+          let listaPesama = document.querySelector  (".song-list");
+          let novaPesma = document.createElement("li");
+          novaPesma.className = "song";
+          novaPesma.innerHTML = `<img src="../images/icon-01.png" alt=""}>
+          <div class="name">
+          <p>${song.artist}</p>
+          <span>${song.title}</span>
+        </div>`;
+        console.log(novaPesma)
+          listaPesama.appendChild(novaPesma);
+        }
       }
-      
     });
   }
-
-  
 }
