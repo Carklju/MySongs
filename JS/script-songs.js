2b5f0829091211649b24d59493db7654da91e8cc
Carklju <65130770+Carklju@users.noreply.github.com>
2023-05-23 08:47:03 +0200

    skripta pesme

--- JS/script-songs.js
+++ JS/script-songs.js
@@ -1,11 +1,22 @@
+fetch("../songs.json")
+  .then((response) => response.json())
+  .then((data) => prikaziPesme(data));
 
+function prikaziPesme(data) {
+  let instrumenti = document.querySelectorAll(".instrument span");
 
-let instrumenti = document.querySelectorAll(".instrument span");
+  for (const item of instrumenti) {
+    item.addEventListener("click", (e) => {
+      let naslov = e.target.innerText;
+      
+      for (let song of data.songs) {
+            console.log("radi");
+            window.location.replace("../HTML/instrument.html");
+            let listaPesama = document.querySelector(".song-list");
+      }
+      
+    });
+  }
 
-for(const item of instrumenti){
-        item.addEventListener("click", (e) =>{
-            let naslov = e.target.innerText;
-            console.log(naslov);
-        })
-    }
-
+  
+}
