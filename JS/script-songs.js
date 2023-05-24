a2c4dbb0283166e3dac6ad3029aaa8e4501bc5d2
Ivica Kljucar <ivicamacbook@Ivicas-MacBook-Air.local>
2023-05-25 01:11:26 +0200

    audio player

--- JS/script-songs.js
+++ JS/script-songs.js
@@ -2,6 +2,8 @@ fetch("../songs.json")
   .then((response) => response.json())
   .then((data) => prikaziPesme(data));
 
+
+//Funkcija koja proverava koje je dugme odnosno instrument je selektovan
 function prikaziPesme(data) {
   let instrumenti = document.querySelectorAll(".instrument span");
   for (const item of instrumenti) {
@@ -11,25 +13,26 @@ function prikaziPesme(data) {
         ispis(data, naslov);
       } else if (naslov === "Guitar") {
         ispis(data, naslov);
-      } else{
+      } else {
         ispis(data, naslov);
       }
     });
   }
 }
 
-function ispis(data, naslov){
+//Funckija koja ispisuje listu pesama u zavisnosti koji je instrument selektovan
+function ispis(data, naslov) {
   let filtrirano = data.songs.filter((song) => {
     return song.instrument === String(naslov);
   });
-  console.log(filtrirano);
   let stranica = document.querySelector(".instruments");
   stranica.style.display = "none";
   for (let song of filtrirano) {
     let listaPesama = document.querySelector(".song-list");
     let novaPesma = document.createElement("li");
     novaPesma.className = "song";
-    novaPesma.innerHTML = `<img src="../images/${song.img}" alt=""}>
+    novaPesma.innerHTML = `
+                              <img src="../images/${song.img}" alt=""}>
                                 <div class="name">
                                   <p>${song.artist}</p>
                                  <span>${song.title}</span>
@@ -39,15 +42,86 @@ function ispis(data, naslov){
                                 </div>
                                 `;
     listaPesama.appendChild(novaPesma);
-    let detaljiPesma = document.createElement("div");
-    detaljiPesma.className = "song-detail";
-    detaljiPesma.innerHTML = `<div>
-     <img src="../images/icon-01.png" alt="">
+  }
+  //---------------------------------------------------------------------
+  let detaljiStrelica = document.querySelectorAll(".more");
+  for (let strelica of detaljiStrelica) {
+      strelica.addEventListener("click", (e) => {
+      let detaljiPesma = document.createElement("div");
+      let imePesme = e.target.parentElement.parentElement.children[1].innerHTML;
+      let filtriranaPesma = data.songs.filter((song) => {
+        return song.title === String(imePesme);
+      });
+      console.log(String(filtriranaPesma[0].title));
+      let pesma = e.target.parentElement.parentElement.parentElement;
+      detaljiPesma.className = "song-detail";
+      detaljiPesma.innerHTML = `<div>
+     <img src="../images/${filtriranaPesma[0].img}" alt="">
      </div>`;
-    listaPesama.appendChild(detaljiPesma);
+      pesma.style.display = "none";
+      pesma.after(detaljiPesma);
+    });
   }
 }
 
+//Funkcije za pustanje pesme u detaljima
+let progress = document.getElementById("progress");
+let pesmaMp3 = document.getElementById("audioMp3");
+let play = document.getElementById("dugmePlay");
+
+pesmaMp3.addEventListener("onloadedmetadata", () => {
+  progress.max = pesmaMp3.duration;
+  progress.value = pesmaMp3.currentTime;
+});
+
+window.addEventListener("load", () =>{
+  pesmaMp3.pause();
+});
+
+play.addEventListener("click", () =>{
+  if(play.classList.contains("fa-pause")){
+    pesmaMp3.pause();
+    play.classList.remove("fa-pause");
+    play.classList.add("fa-play");
+  }else{
+    pesmaMp3.play();
+    play.classList.remove("fa-play");
+    play.classList.add("fa-pause");
+  }
+});
+
+document.addEventListener("keyup", (e) =>{
+  if(e.code === "Space"){
+    if(play.classList.contains("fa-pause")){
+      pesmaMp3.pause();
+      play.classList.remove("fa-pause");
+      play.classList.add("fa-play");
+    }else{
+      pesmaMp3.play();
+      play.classList.remove("fa-play");
+      play.classList.add("fa-pause");
+    }
+  }
+})
+
+// if(pesmaMp3.play()){
+//   setInterval(()=>{
+     //progress.max = pesmaMp3.duration;
+//     progress.value = pesmaMp3.currentTime;
+   //currentTime.innerHTML = formatTime(pesmaMp3.currentTime);
+//   }, 1500);
+// }
+
+pesmaMp3.addEventListener("timeupdate", () =>{
+  progress.value = pesmaMp3.currentTime / pesmaMp3.duration * 100;
+})
+
+progress.addEventListener("change", ()=>{
+  pesmaMp3.play();
+  pesmaMp3.currentTime = progress.value;
+  play.classList.remove("fa-play");
+  play.classList.add("fa-pause");
+});
 
 
 
