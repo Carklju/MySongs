d3a3b43bbf040a63597e550c2d884da47201e223
Ivica Kljucar <ivicamacbook@Ivicas-MacBook-Air.local>
2023-05-25 18:43:42 +0200

    update

--- JS/script-songs.js
+++ JS/script-songs.js
@@ -2,7 +2,6 @@ fetch("../songs.json")
   .then((response) => response.json())
   .then((data) => prikaziPesme(data));
 
-
 //Funkcija koja proverava koje je dugme odnosno instrument je selektovan
 function prikaziPesme(data) {
   let instrumenti = document.querySelectorAll(".instrument span");
@@ -43,85 +42,190 @@ function ispis(data, naslov) {
                                 `;
     listaPesama.appendChild(novaPesma);
   }
-  //---------------------------------------------------------------------
+  ispisDetalja(data)
+  //Ispis detalja 
+  // let detaljiStrelica = document.querySelectorAll(".more");
+  // for (let strelica of detaljiStrelica) {
+  //   strelica.addEventListener("click", (e) => {
+  //     let detaljiPesma = document.createElement("div"); 
+  //     let imePesme = e.target.parentElement.parentElement.children[1].innerHTML;
+  //     let filtriranaPesma = data.songs.filter((song) => {
+  //       return song.title === String(imePesme);
+  //     });
+  //     let pesma = e.target.parentElement.parentElement.parentElement;
+  //     detaljiPesma.className = "song-detail";
+  //     detaljiPesma.innerHTML = `
+  //    <img src="../images/${filtriranaPesma[0].img}" alt="">
+  //    <div class="song-details-info">
+  //         <p><span>Title: </span>${filtriranaPesma[0].title}</p>
+  //         <p><span>Artist: </span>${filtriranaPesma[0].artist}</p>
+  //         <p><span>Album: </span>${filtriranaPesma[0].album}</p>
+  //         <p><span>Release Date: </span>${filtriranaPesma[0].releaseDate}</p>
+  //         <p><span>Duration: </span>${filtriranaPesma[0].duration}</p>
+  //         <p><span>Genre: </span>${filtriranaPesma[0].genre}</p>
+  //         <p><span>Language: </span>${filtriranaPesma[0].language}</p>
+  //         <audio control id="audioMp3">
+  //           <source src="../media/${filtriranaPesma[0].mp3}"
+  //           type="audio/mpeg">
+  //         </audio>
+  //         <input type="range" value="0" id="progress">
+  //         <div class="controls">
+  //           <div><i class="fa-solid fa-play" id="dugmePlay"></i></div>
+  //         </div>
+  //         <div class="show-less">
+  //         <i id="StrelicaGore" class="material-icons">expand_more</i>
+  //       </div>
+  //     </div>
+  //    `;
+  //     pesma.style.display = "none";
+  //     pesma.after(detaljiPesma);
+    
+  //     //Funkcije za pustanje pesme u detaljima
+  //     let progress = document.getElementById("progress");
+  //     var pesmaMp3 = document.getElementById("audioMp3");
+  //     let play = document.getElementById("dugmePlay");
+
+  //     pesmaMp3.addEventListener("onloadedmetadata", (e) => {
+  //       progress.max = pesmaMp3.duration;
+  //       progress.value = pesmaMp3.currentTime;
+  //     });
+
+  //     window.addEventListener("load", () => {
+  //       pesmaMp3.pause();
+  //     });
+
+  //     play.addEventListener("click", () => {
+  //       if (play.classList.contains("fa-pause")) {
+  //         pesmaMp3.pause();
+  //         play.classList.remove("fa-pause");
+  //         play.classList.add("fa-play");
+  //       } else {
+  //         pesmaMp3.play();
+  //         play.classList.remove("fa-play");
+  //         play.classList.add("fa-pause");
+  //       }
+  //     });
+
+  //     document.addEventListener("keyup", (e) => {
+  //       if (e.code === "Space" && e.target == document.body) {
+  //         e.preventDefault();
+  //         if (play.classList.contains("fa-pause")) {
+  //           pesmaMp3.pause();
+  //           play.classList.remove("fa-pause");
+  //           play.classList.add("fa-play");
+  //         } else {
+  //           pesmaMp3.play();
+  //           play.classList.remove("fa-play");
+  //           play.classList.add("fa-pause");
+  //         }
+  //       }
+  //     });
+
+  //     pesmaMp3.addEventListener("timeupdate", () => {
+  //       progress.value = (pesmaMp3.currentTime / pesmaMp3.duration) * 100;
+  //     });
+
+  //     progress.addEventListener("change", () => {
+  //       pesmaMp3.play();
+  //       pesmaMp3.currentTime = progress.value;
+  //       play.classList.remove("fa-play");
+  //       play.classList.add("fa-pause");
+  //     });
+  //   });
+
+  // }
+}
+
+
+function ispisDetalja(data){
   let detaljiStrelica = document.querySelectorAll(".more");
   for (let strelica of detaljiStrelica) {
-      strelica.addEventListener("click", (e) => {
-      let detaljiPesma = document.createElement("div");
+    strelica.addEventListener("click", (e) => {
+      let detaljiPesma = document.createElement("div"); 
       let imePesme = e.target.parentElement.parentElement.children[1].innerHTML;
       let filtriranaPesma = data.songs.filter((song) => {
         return song.title === String(imePesme);
       });
-      console.log(String(filtriranaPesma[0].title));
       let pesma = e.target.parentElement.parentElement.parentElement;
       detaljiPesma.className = "song-detail";
-      detaljiPesma.innerHTML = `<div>
+      detaljiPesma.innerHTML = `
      <img src="../images/${filtriranaPesma[0].img}" alt="">
-     </div>`;
+     <div class="song-details-info">
+          <p><span>Title: </span>${filtriranaPesma[0].title}</p>
+          <p><span>Artist: </span>${filtriranaPesma[0].artist}</p>
+          <p><span>Album: </span>${filtriranaPesma[0].album}</p>
+          <p><span>Release Date: </span>${filtriranaPesma[0].releaseDate}</p>
+          <p><span>Duration: </span>${filtriranaPesma[0].duration}</p>
+          <p><span>Genre: </span>${filtriranaPesma[0].genre}</p>
+          <p><span>Language: </span>${filtriranaPesma[0].language}</p>
+          <audio control id="audioMp3">
+            <source src="../media/${filtriranaPesma[0].mp3}"
+            type="audio/mpeg">
+          </audio>
+          <input type="range" value="0" id="progress">
+          <div class="controls">
+            <div><i class="fa-solid fa-play" id="dugmePlay"></i></div>
+          </div>
+          <div class="show-less">
+          <i id="StrelicaGore" class="material-icons">expand_more</i>
+        </div>
+      </div>
+     `;
       pesma.style.display = "none";
       pesma.after(detaljiPesma);
+    
+      //Funkcije za pustanje pesme u detaljima
+      let progress = document.getElementById("progress");
+      var pesmaMp3 = document.getElementById("audioMp3");
+      let play = document.getElementById("dugmePlay");
+
+      pesmaMp3.addEventListener("onloadedmetadata", (e) => {
+        progress.max = pesmaMp3.duration;
+        progress.value = pesmaMp3.currentTime;
+      });
+
+      window.addEventListener("load", () => {
+        pesmaMp3.pause();
+      });
+
+      play.addEventListener("click", () => {
+        if (play.classList.contains("fa-pause")) {
+          pesmaMp3.pause();
+          play.classList.remove("fa-pause");
+          play.classList.add("fa-play");
+        } else {
+          pesmaMp3.play();
+          play.classList.remove("fa-play");
+          play.classList.add("fa-pause");
+        }
+      });
+
+      document.addEventListener("keyup", (e) => {
+        if (e.code === "Space" && e.target == document.body) {
+          e.preventDefault();
+          if (play.classList.contains("fa-pause")) {
+            pesmaMp3.pause();
+            play.classList.remove("fa-pause");
+            play.classList.add("fa-play");
+          } else {
+            pesmaMp3.play();
+            play.classList.remove("fa-play");
+            play.classList.add("fa-pause");
+          }
+        }
+      });
+
+      pesmaMp3.addEventListener("timeupdate", () => {
+        progress.value = (pesmaMp3.currentTime / pesmaMp3.duration) * 100;
+      });
+
+      progress.addEventListener("change", () => {
+        pesmaMp3.play();
+        pesmaMp3.currentTime = progress.value;
+        play.classList.remove("fa-play");
+        play.classList.add("fa-pause");
+      });
     });
   }
 }
-
-//Funkcije za pustanje pesme u detaljima
-let progress = document.getElementById("progress");
-let pesmaMp3 = document.getElementById("audioMp3");
-let play = document.getElementById("dugmePlay");
-
-pesmaMp3.addEventListener("onloadedmetadata", () => {
-  progress.max = pesmaMp3.duration;
-  progress.value = pesmaMp3.currentTime;
-});
-
-window.addEventListener("load", () =>{
-  pesmaMp3.pause();
-});
-
-play.addEventListener("click", () =>{
-  if(play.classList.contains("fa-pause")){
-    pesmaMp3.pause();
-    play.classList.remove("fa-pause");
-    play.classList.add("fa-play");
-  }else{
-    pesmaMp3.play();
-    play.classList.remove("fa-play");
-    play.classList.add("fa-pause");
-  }
-});
-
-document.addEventListener("keyup", (e) =>{
-  if(e.code === "Space"){
-    if(play.classList.contains("fa-pause")){
-      pesmaMp3.pause();
-      play.classList.remove("fa-pause");
-      play.classList.add("fa-play");
-    }else{
-      pesmaMp3.play();
-      play.classList.remove("fa-play");
-      play.classList.add("fa-pause");
-    }
-  }
-})
-
-// if(pesmaMp3.play()){
-//   setInterval(()=>{
-     //progress.max = pesmaMp3.duration;
-//     progress.value = pesmaMp3.currentTime;
-   //currentTime.innerHTML = formatTime(pesmaMp3.currentTime);
-//   }, 1500);
-// }
-
-pesmaMp3.addEventListener("timeupdate", () =>{
-  progress.value = pesmaMp3.currentTime / pesmaMp3.duration * 100;
-})
-
-progress.addEventListener("change", ()=>{
-  pesmaMp3.play();
-  pesmaMp3.currentTime = progress.value;
-  play.classList.remove("fa-play");
-  play.classList.add("fa-pause");
-});
-
-
 
